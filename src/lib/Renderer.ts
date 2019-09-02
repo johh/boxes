a73886c55f18eda3b4ec4002dc68a10012476c73
johh <code@johh.net>
2019-09-02 23:08:47 +0200

    FEATURE: fill texture slot 0 with debug texture

--- src/lib/Renderer.ts
+++ src/lib/Renderer.ts
@@ -13,6 +13,7 @@ export default class Renderer {
 	constructor( props: RendererPops ) {
 		this.gl = props.canvas.getContext( 'webgl' );
 
+		this.createDebugTexture();
 		this.setSize( 800, 600 );
 	}
 
@@ -31,5 +32,38 @@ export default class Renderer {
 
 		material.use( this.gl );
 		geometry.draw( this.gl );
+	}
+
+	private createDebugTexture() {
+		const texture = this.gl.createTexture();
+		this.gl.bindTexture( this.gl.TEXTURE_2D, texture );
+
+		this.gl.texImage2D(
+			this.gl.TEXTURE_2D,
+			0,
+			this.gl.RGBA,
+			2,
+			2,
+			0,
+			this.gl.RGBA,
+			this.gl.UNSIGNED_BYTE,
+			new Uint8Array([
+				1, 0, 1, 1,
+				0, 0, 0, 1,
+				0, 0, 0, 1,
+				1, 0, 1, 1,
+			].map( v => v * 255 ) ),
+		);
+
+		this.gl.texParameteri(
+			this.gl.TEXTURE_2D,
+			this.gl.TEXTURE_MIN_FILTER,
+			this.gl.NEAREST,
+		);
+		this.gl.texParameteri(
+			this.gl.TEXTURE_2D,
+			this.gl.TEXTURE_MAG_FILTER,
+			this.gl.NEAREST,
+		);
 	}
 }

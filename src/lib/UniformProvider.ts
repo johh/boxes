9da414bb0884697bc9199dfc6074ccc48b4df5ce
johh <code@johh.net>
2019-10-23 22:52:58 +0200

    TASK: UniformProvider: update methods
    Added update methods to keep API in sync with Material

--- src/lib/UniformProvider.ts
+++ src/lib/UniformProvider.ts
@@ -1,11 +1,21 @@
 import Traversable from './Traversable';
-import { UniformList } from './Material';
 import Renderable from './Renderable';
+import { UniformList } from './Material';
 import { UniformValue } from './UniformValue';
+
+
+type UniformProivderUpdateFunction =
+	( previousValue: UniformValue ) => UniformValue | void;
+
+interface UniformProivderUpdateList {
+	[key: string]: UniformProivderUpdateFunction;
+}
+
 
 interface UniformProivderProps {
 	uniforms: UniformList;
 }
+
 
 export default class UniformProivder implements Traversable {
 	public readonly isUniformProvider = true;
@@ -32,6 +42,18 @@ export default class UniformProivder implements Traversable {
 	public setUniforms( list: UniformList ) {
 		Object.keys( list ).forEach( ( key ) => {
 			this.setUniform( key, list[key]);
+		});
+	}
+
+
+	public updateUniform( key: string, func: UniformProivderUpdateFunction ) {
+		this.uniforms[key] = func( this.uniforms[key]) || this.uniforms[key];
+	}
+
+
+	public updateUniforms( list: UniformProivderUpdateList ) {
+		Object.keys( list ).forEach( ( key ) => {
+			this.updateUniform( key, list[key]);
 		});
 	}
 

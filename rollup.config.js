02c1ab0842a2ecdb1f58c1562040f5dd9570eadc
johh <code@johh.net>
2020-08-18 17:42:42 +0200

    chore(build): provide cjs version

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,18 @@ import typescript from 'rollup-plugin-typescript2';
 export default {
 	input: './src/boxes.ts',
 
-	output: {
-		file: 'dist/boxes.js',
-		format: 'esm'
-	},
+	output: [
+		{
+			file: 'dist/esm/boxes.js',
+			format: 'esm',
+		},
+		{
+			file: 'dist/cjs/boxes.js',
+			format: 'cjs',
+		},
+	],
 
 	plugins: [
 		typescript(),
 	],
-}
+};
